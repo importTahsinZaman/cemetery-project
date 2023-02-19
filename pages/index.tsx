70cb3769a8c7a344053ff58a5527c769d008aee3
importTahsinZaman <tahsinz21366@gmail.com>
2023-02-19 14:51:04 -0500

    Added Navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,5 +5,10 @@ import { Inter } from "@next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <div className="bg-red-50"></div>;
+  return (
+    <>
+      <div className="min-h-screen min-w-screen bg-[#F9F3E5]"></div>
+      <div className="min-h-screen min-w-screen bg-blue-100"></div>
+    </>
+  );
 }
