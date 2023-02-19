e293b9b63f0f5478f235bcec3d2598fa0568b437
importTahsinZaman <tahsinz21366@gmail.com>
2023-02-19 14:57:41 -0500

    Fixed Link components in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,22 +5,22 @@ import { useContext, useState } from "react";
 export default function Navbar() {
   const [navbar, setNavbar] = useState(false);
   return (
-    <nav className="w-full bg-gray-800 shadow">
-      <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
+    <nav className="w-full bg-gray-800">
+      <div className="mx-auto justify-between  px-4 md:flex md:items-center md:px-8 lg:max-w-7xl">
         <div>
-          <div className="flex items-center justify-between py-3 md:py-5 md:block">
+          <div className="flex items-center justify-between py-3 md:block md:py-5">
             <a href="#">
-              <h2 className="text-2xl text-white font-bold">NEXT JS</h2>
+              <h2 className="text-2xl font-bold text-white">NEXT JS</h2>
             </a>
             <div className="md:hidden">
               <button
-                className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+                className="rounded-md p-2 text-gray-700 outline-none focus:border focus:border-gray-400"
                 onClick={() => setNavbar(!navbar)}
               >
                 {navbar ? (
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    className="w-6 h-6 text-white"
+                    className="h-6 w-6 text-white"
                     viewBox="0 0 20 20"
                     fill="currentColor"
                   >
@@ -33,7 +33,7 @@ export default function Navbar() {
                 ) : (
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
-                    className="w-6 h-6 text-white"
+                    className="h-6 w-6 text-white"
                     fill="none"
                     viewBox="0 0 24 24"
                     stroke="currentColor"
@@ -52,30 +52,22 @@ export default function Navbar() {
         </div>
         <div>
           <div
-            className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+            className={`mt-8 flex-1 justify-self-center pb-3 md:mt-0 md:block md:pb-0 ${
               navbar ? "block" : "hidden"
             }`}
           >
             <ul className="items-center justify-center space-y-8 md:flex md:space-x-6 md:space-y-0">
               <li className="text-white">
-                <Link legacyBehavior href="/">
-                  <a>Home</a>
-                </Link>
+                <Link href="/">Home</Link>
               </li>
               <li className="text-white">
-                <Link legacyBehavior href="/blogs">
-                  <a>Blogs</a>
-                </Link>
+                <Link href="/blogs"> Blogs</Link>
               </li>
               <li className="text-white">
-                <Link legacyBehavior href="/about">
-                  <a>About US</a>
-                </Link>
+                <Link href="/about"> About</Link>
               </li>
               <li className="text-white">
-                <Link legacyBehavior href="/contact">
-                  <a>Contact US</a>
-                </Link>
+                <Link href="/contact"> Contact Us</Link>
               </li>
             </ul>
           </div>
