91245f7d8ba22838b72d41e7a2e5d6d4b846a4c7
importTahsinZaman <tahsinz21366@gmail.com>
2023-02-19 23:47:14 -0500

    kinda happy with design??

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,29 +1,34 @@
 import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "@next/font/google";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-      <div className="min-w-screen flex min-h-screen grow flex-row  bg-[#e9d8c2]">
+      <div className="min-w-screen flex min-h-screen grow flex-row  border-b-2 border-black bg-[#e9d8c2]">
         <div className="flex min-w-[50%] max-w-[50%] items-center justify-center">
-          <div className="flex  flex-col items-center p-10 text-black">
-            <h1 className="my-5 text-9xl">
-              John A. Higgons
+          <div className="flex flex-col items-center p-10 ">
+            <div className="text-black">
+              <h1 className="my-5 text-9xl">John A. Higgons</h1>
               <h2 className="mt-2 text-3xl">1863 - 1951</h2>
-            </h1>
-            <p className="text-lg">
-              While the genealogy of John Axford Higgons’s ancestors remains
-              relatively obscure, it is known that his family originated from
-              the British Isles. Immigration was present in both his paternal
-              and maternal lineages, as his father was an English immigrant and
-              his mother, though born in New York, was the child of English
-              immigrants. The surname ‘Higgons’ is most likely a mutation of the
-              surname ‘Higgins.’ The surname Higgins is commonly linked to
-              Ireland or the areas affected by the Norman Conquest.{" "}
-            </p>
+              <p className="text-lg">
+                While the genealogy of John Axford Higgons' ancestors remains
+                relatively obscure, it is known that his family originated from
+                the British Isles. Immigration was present in both his paternal
+                and maternal lineages, as his father was an English immigrant
+                and his mother, though born in New York, was the child of
+                English immigrants. The surname ‘Higgons’ is most likely a
+                mutation of the surname ‘Higgins.’ The surname Higgins is
+                commonly linked to Ireland or the areas affected by the Norman
+                Conquest...
+              </p>
+              <Link className="underline" href="/blogs">
+                Read Biography
+              </Link>
+            </div>
           </div>
         </div>
         <div className="flex min-w-[50%] max-w-[50%] items-center justify-center border-l-2 border-black bg-[#fef9f3]">
