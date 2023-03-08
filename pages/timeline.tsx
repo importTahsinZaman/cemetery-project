992daa731c2de4aef5ef0113b0505992ba1038cb
importTahsinZaman <tahsinz21366@gmail.com>
2023-03-08 00:22:27 -0500

    Added timeline

--- pages/timeline.tsx
+++ pages/timeline.tsx
@@ -1,3 +1,10 @@
 export default function timeline() {
-  return <></>;
+  return (
+    <iframe
+      className="h-[100vh]"
+      src="https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=1LnwogYjXlV3WjsXrCoMVkqb4PFkx9pH7jA7ekb6knrU&font=Default&lang=en&initial_zoom=2&height=650"
+      width="100%"
+      height="100%"
+    ></iframe>
+  );
 }
