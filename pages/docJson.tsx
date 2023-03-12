30d41f4a0dd2c0dc0e0e9314a3e8e30ba863b13c
importTahsinZaman <tahsinz21366@gmail.com>
2023-03-12 14:07:58 -0400

    Added documents json export file

---
+++ pages/docJson.tsx
@@ -0,0 +1,21 @@
+export const docJson = [
+  {
+    img: "https://lh6.googleusercontent.com/U7DB1v_yBibal00U0FKpWdA_IeWfyCotns8E3ENPRTREO6Fb5WXul69Xpl4CHgsRjPxefQxXZwxvg6c-NrlQQzitI50ez4_UIW8vJcyFr0SDdnCfNsiy-5IgLhl3RNEOA8nrhyAfIVC2gzqS9B7VL3Q",
+    caption: "1854 US Presbyterian Church Marriage Records",
+    transcription: "Jan. 18th, Oliver Harvey Willard to Rebecca Ann Berstler",
+    citation:
+      "Third Presbyterian Church. “Presbyterian Church Records.” www.ancestry.com, January 18, 1854. https://www.ancestry.com/discoveryui-content/view/1500969850:61048?tid=&pid=&queryId=6b50a405e0ac14f11da1763366e28c38&_phsrc=klS180&_phstart=successSource.",
+    analysis:
+      "According to the U.S. Presbyterian Church Marriage Records, Oliver Harvey Willard and Rebecca Ann Berstler were married on January 18, 1854.  This is from the Third Presbyterian Church in Philadelphia.<br/><br /> It can be inferred that Oliver Harvey Willard and Rebecca Ann Berstler were religious, as their marriage record is kept in a presbyterian church database. ",
+  },
+  {
+    img: "https://lh3.googleusercontent.com/TLHva60yIh7ilhNpvk8U7a9hr-AloP3gq5uFICX4QdIK03y8XkXKax1c1_qQz0C-ObItEO1xSf8pe9LWhmV-G3GWfFq0VyEHZgMtBXIno007fGla2H1ahLtsm-wU1qCyDRliod_g9d7ewyJFCiWci4I",
+    caption: "Pennsylvania and New Jersey, U.S., Church and Town Records, 1856",
+    transcription:
+      "Name: | Oliver Willard | Residence Date: 1856 | Residence Place | Philadelphia, Pennsylvania",
+    citation:
+      "Philadelphia Saving Fund Society. “Pennsylvania and New Jersey, U.S., Church and Town Records.” www.ancestry.com, 1856. https://www.ancestry.com/discoveryui-content/view/2020776832:2451?tid=&pid=&queryId=511a74377ec978182e88366364bd356c&_phsrc=klS25&_phstart=successSource.",
+    analysis:
+      "The Pennsylvania and New Jersey Church and Town Records for the year 1856 place Oliver H. Willard, John Axford Higgons’s father-in-law, in Philadelphia at the time.<br/><br/>It can be inferred from this document that Rebecca Ann Berstler, Willard’s wife, also lived in Philadelphia at the time, because it was common for husband and wife to live in the same residence. In addition, it can be inferred that their child, Sarah Anne Willard, who was born two years earlier, lived with Willard in Philadelphia. ",
+  },
+];
--- pages/documents.tsx
+++ pages/documents.tsx
@@ -2,31 +2,9 @@ import Image from "next/image";
 import { useState } from "react";
 import Swipe from "react-easy-swipe";
 import { AiOutlineLeft, AiOutlineRight } from "react-icons/ai";
+import { docJson } from "./docJson";
 
 export default function documents() {
-  const documents = [
-    {
-      img: "https://lh6.googleusercontent.com/U7DB1v_yBibal00U0FKpWdA_IeWfyCotns8E3ENPRTREO6Fb5WXul69Xpl4CHgsRjPxefQxXZwxvg6c-NrlQQzitI50ez4_UIW8vJcyFr0SDdnCfNsiy-5IgLhl3RNEOA8nrhyAfIVC2gzqS9B7VL3Q",
-      caption: "1854 US Presbyterian Church Marriage Records",
-      transcription: "Jan. 18th, Oliver Harvey Willard to Rebecca Ann Berstler",
-      citation:
-        "Third Presbyterian Church. “Presbyterian Church Records.” www.ancestry.com, January 18, 1854. https://www.ancestry.com/discoveryui-content/view/1500969850:61048?tid=&pid=&queryId=6b50a405e0ac14f11da1763366e28c38&_phsrc=klS180&_phstart=successSource.",
-      analysis:
-        "According to the U.S. Presbyterian Church Marriage Records, Oliver Harvey Willard and Rebecca Ann Berstler were married on January 18, 1854.  This is from the Third Presbyterian Church in Philadelphia.<br/><br /> It can be inferred that Oliver Harvey Willard and Rebecca Ann Berstler were religious, as their marriage record is kept in a presbyterian church database. ",
-    },
-    {
-      img: "https://lh3.googleusercontent.com/TLHva60yIh7ilhNpvk8U7a9hr-AloP3gq5uFICX4QdIK03y8XkXKax1c1_qQz0C-ObItEO1xSf8pe9LWhmV-G3GWfFq0VyEHZgMtBXIno007fGla2H1ahLtsm-wU1qCyDRliod_g9d7ewyJFCiWci4I",
-      caption:
-        "Pennsylvania and New Jersey, U.S., Church and Town Records, 1856",
-      transcription:
-        "Name: | Oliver Willard | Residence Date: 1856 | Residence Place | Philadelphia, Pennsylvania",
-      citation:
-        "Philadelphia Saving Fund Society. “Pennsylvania and New Jersey, U.S., Church and Town Records.” www.ancestry.com, 1856. https://www.ancestry.com/discoveryui-content/view/2020776832:2451?tid=&pid=&queryId=511a74377ec978182e88366364bd356c&_phsrc=klS25&_phstart=successSource.",
-      analysis:
-        "The Pennsylvania and New Jersey Church and Town Records for the year 1856 place Oliver H. Willard, John Axford Higgons’s father-in-law, in Philadelphia at the time.<br/><br/>It can be inferred from this document that Rebecca Ann Berstler, Willard’s wife, also lived in Philadelphia at the time, because it was common for husband and wife to live in the same residence. In addition, it can be inferred that their child, Sarah Anne Willard, who was born two years earlier, lived with Willard in Philadelphia. ",
-    },
-  ];
-
   const [currentSlide, setCurrentSlide] = useState(0);
 
   const handleNextSlide = () => {
@@ -106,6 +84,8 @@ export default function documents() {
     }
   }
 
+  console.log(docJson);
+
   console.log(numberToEncodedLetter(27));
 
   return (
@@ -123,7 +103,7 @@ export default function documents() {
           onSwipeRight={handlePrevSlide}
           className="relative z-10 h-full w-full"
         >
-          {documents.map((content: any, index: number) => {
+          {docJson.map((content: any, index: number) => {
             if (index === currentSlide) {
               return (
                 <div
@@ -167,7 +147,7 @@ export default function documents() {
       />
 
       <div className="absolute left-[50%] bottom-[2%] z-20 flex translate-x-[-50%] justify-center self-center p-2">
-        {documents.map((_: any, index: number) => {
+        {docJson.map((_: any, index: number) => {
           return (
             <div
               className={
