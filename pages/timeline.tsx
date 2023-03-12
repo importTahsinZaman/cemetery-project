389d1ac3cc985ff11b15ff4dcf8466f0ba924cdc
importTahsinZaman <tahsinz21366@gmail.com>
2023-03-11 21:34:57 -0500

    Design Save

--- pages/timeline.tsx
+++ pages/timeline.tsx
@@ -1,6 +1,6 @@
 export default function timeline() {
   return (
-    <div className="flex h-[100vh] w-[100vw] items-center justify-center">
+    <div className=" fixed flex h-screen w-[100vw] translate-y-[14%]">
       <iframe
         className="h-[100vh] object-center"
         src="https://cdn.knightlab.com/libs/timeline3/latest/embed/index.html?source=1LnwogYjXlV3WjsXrCoMVkqb4PFkx9pH7jA7ekb6knrU&font=Default&lang=en&initial_zoom=2&height=650"
